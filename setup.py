c3134800e838158439e04aed8a40332755e46347
DementiaGaming <sukhrathansidhu@outlook.com>
2025-03-31 21:09:46 +0100

    Bump version to V1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="listConversions",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[],  # Add dependencies here
     author="Dementia Gaming",
